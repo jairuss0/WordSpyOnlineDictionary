8c8cd435c5cb395e4251e843e6e77e3b1e26b49b
jairuss0 <elijahjairusc@gmail.com>
2024-05-10 00:29:42 +0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+
+// load initial games
+document.addEventListener("DOMContentLoaded", () => {
+    
+  getDataResponse();
+        
+});
+
+// get the response
+async function getDataResponse(){
+    try{
+        const dataResponse = await getApiResponse("fast");
+        seeData(dataResponse);
+    }catch(error){
+        console.error(error);
+    }
+}
+// fetch the api
+async function getApiResponse(word){
+    try{
+        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        if(!response.ok){
+           console.log("Error bitch");
+        }
+        
+        return await response.json();
+
+    }catch(error){
+        console.error(error);
+    }
+}
+
+function seeData(data){
+    console.log(data);
+}
